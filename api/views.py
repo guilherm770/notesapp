187c44a22fc7d83eaea92ed187a1da857588bcf1
guilherme <guilherme.rezende@redacaonline.com.br>
2023-11-03 10:46:08 -0300

    rest framework and its views url and serializers, config as explained in docs

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
 
     routes = [
@@ -38,4 +42,16 @@ def getRoutes(request):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    note = Note.objects.all()
+    serializer = NoteSerializer(note, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
